94140d2835bf8b74fcd213fac80b2f24008dfeb2
Michael Vorburger <mike@vorburger.ch>
2011-04-07 18:48:01 +0200

    JavaDoc only

--- appviewsnflows-core/src/main/java/ch/vorburger/appviewsnflows/Event.java
+++ appviewsnflows-core/src/main/java/ch/vorburger/appviewsnflows/Event.java
@@ -3,24 +3,25 @@ package ch.vorburger.appviewsnflows;
 /**
  * Event - something that "happens" in a View.
  * 
- * The key for what happened is determined by specific type-safe subclasses of
- * this Event.
+ * <p>The key for what happened is determined by specific type-safe subclasses of
+ * this Event (unless the {@link ch.vorburger.appviewsnflows.dyn.Event} child
+ * interface is used).</p>
  * 
- * Events typically carry data that provide arbitrary payload parameters
+ * <p>Events typically carry data that provide arbitrary payload parameters
  * detailing the event (e.g. what was clicked on). For the most typical case of
- * Event with exactly one parameter, see the Event1 class.
+ * Event with exactly one parameter, see the {@link Event1} class.</p>
  * 
- * The full View object in which the event occurred is not part of the Event, as
+ * <p>The full View object in which the event occurred is not part of the Event, as
  * the Flow when handling the Event already has this, and it should normally not
- * be used / needed to determining where to go anyways.
+ * be used / needed to determining where to go anyways.</p>
  * 
- * This intentionally does not sub-class the java.util.EventObject, because that
+ * <p>This intentionally does not sub-class the java.util.EventObject, because that
  * is not type safe (generics), and because it's terminology ("sender") is less
- * suitable for this purpose.
+ * suitable for this purpose.</p>
  * 
- * There is no "Listener" for these simple Events, it's not a classical Observer
+ * <p>There is no "Listener" for these simple Events, it's not a classical Observer
  * pattern, as its only purpose is to transport data from within View through to
- * Flow logic.
+ * Flow logic.</p>
  * 
  * @see Flow#onEvent(Event)
  * @author Michael Vorburger
